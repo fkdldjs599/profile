61bbd72e08f951c9d1a8d1d2918dd32194be0982
fkdldjs599 <fkdldjs599@gmail.com>
2025-11-10 15:41:28 +0900

    commit15

--- js/js.js
+++ js/js.js
@@ -1,237 +1,233 @@
-$(document).ready(function(){
+$(window).on('load', function(){
 
-    var section1 = $('.sc1').offset().top;
-    var section2 = $('.sc2').offset().top;
-    var section4 = $('.sc4 .category').offset().top;
-    var section5 = $('.sc5').offset().top;
-
-    $('.sc1 .view_btn').mouseenter(function(){
-        $('.view_btn .hoverbg').stop().animate({'width': '100%'}, 300);
-        $('.view_btn').css({'color': 'white'}, 300);
-    });
-    $('.sc1 .view_btn').mouseleave(function(){
-        $('.view_btn .hoverbg').stop().animate({'width': '0%'}, 300);
-        $('.view_btn').css({'color': '#2C3A3A'}, 300);
-    });
-
-    $(window).scroll(function () {
-        var scrollY = ($(window).scrollTop() / ($(document).height() - $(window).height()) * 100).toFixed(3);
-        $(".sc_color").css({"width": scrollY + "%"});
-      });
-
-    $(document).scroll(function(){
-        
-        $('.nav_a').removeClass('on');
-        var scrollTop = $(document).scrollTop();
-        
-        if (scrollTop > 0) {
-            $('.header').addClass('fixed');
-        }else{
-            $('.header').removeClass('fixed');
-        }
-
-        if(scrollTop > section5 - 50) {
-            $('.nav_a').addClass('white');
-        }else{
-            $('.nav_a').removeClass('white');
-        }
-
-        if(scrollTop > section5 - 60){
-            $('.nv4').addClass('on');
-
-        }else if(scrollTop > $('.sc4').offset().top - 50){
-            $('.nv3').addClass('on');
-
-        }else if(scrollTop > section2 - 60){
-            $('.nv2').addClass('on');
-
-        }else if(scrollTop > section1){
-            $('.nv1').addClass('on');
-
-        }else if(scrollTop == '0'){
+        var section1 = $('.sc1').offset().top;
+        var section2 = $('.sc2').offset().top;
+        var section4 = $('.sc4 .category').offset().top;
+        var section5 = $('.sc5').offset().top;
+    
+        $('.sc1 .view_btn').mouseenter(function(){
+            $('.view_btn .hoverbg').stop().animate({'width': '100%'}, 300);
+            $('.view_btn').css({'color': 'white'}, 300);
+        });
+        $('.sc1 .view_btn').mouseleave(function(){
+            $('.view_btn .hoverbg').stop().animate({'width': '0%'}, 300);
+            $('.view_btn').css({'color': '#2C3A3A'}, 300);
+        });
+    
+        $(window).scroll(function () {
+            var scrollY = ($(window).scrollTop() / ($(document).height() - $(window).height()) * 100).toFixed(3);
+            $(".sc_color").css({"width": scrollY + "%"});
+          });
+    
+        $(document).scroll(function(){
+            
             $('.nav_a').removeClass('on');
-        }
-
-    });
-
-    $('.header .logo_a').click(function(){
-        $('body, html').stop().animate({scrollTop: '0'}, 500);
-        return false;
-    });
-
-    $('.header .nav_li').click(function(){
-        var idx = $(this).index();
-
-
-        if(idx == '0'){
-            $('body, html').stop().animate({scrollTop: 0}, 500);
-        }else if(idx == '1'){
-            $('body, html').stop().animate({scrollTop: section2 - 50 + 'px'}, 500);
-        }else if(idx == '2'){
-            $('body, html').stop().animate({scrollTop: section4 - 120 + 'px'}, 500);
-        }else if(idx == '3'){
-            $('body, html').stop().animate({scrollTop: section5 - 50 + 'px'}, 500);
-        }
-
-        return false;
-    });
-
-    $('.sc1 .view_btn').click(function(){
-        $('body, html').stop().animate({scrollTop: section4 - 120 + 'px'}, 1000);
-    });
-
-
-    $('.sc4 .port_box .btn').mouseenter(function(){
-        $(this).find('.bar').stop().animate({'width': '55%'}, 300);
-    });
-    $('.sc4 .port_box .btn').mouseleave(function(){
-        $(this).find('.bar').stop().animate({'width': '0%'}, 300);
-    });
-
-
-    $('.port_box .img_area, .port_box .text_area').click(function(){
-        if($(this).parent().hasClass('exit')){
-            location.href='exit.html';
-        }
-        else if($(this).parent().hasClass('nest')){
-            location.href='nest.html';
-        }
-        else if($(this).parent().hasClass('medifine')){
-            location.href='medifine.html';
-        }
-        else if($(this).parent().hasClass('lawpaper')){
-            location.href='lawpaper.html';
-        }
-        else if($(this).parent().hasClass('smmakers')){
-            location.href='smmakers.html';
-        }
-        else if($(this).parent().hasClass('moja')){
-            location.href='moja.html';
-        }
-        else if($(this).parent().hasClass('incom')){
-            location.href='incom.html';
-        }
-        else if($(this).parent().hasClass('gain')){
-            location.href='gain.html';
-        }
-        else if($(this).parent().hasClass('trip')){
-            location.href='trip.html';
-        }
-        else if($(this).parent().hasClass('detail')){
-            location.href='detail.html';
-        }
-        else if($(this).parent().hasClass('design')){
-            location.href='design.html';
-        }
-        else if($(this).parent().hasClass('hook')){
-            location.href='hook.html';
-        }
-
-        return false;
-    });
-
-    $('.sc4 .category .cate_a').click(function(){
+            var scrollTop = $(document).scrollTop();
+            
+            if (scrollTop > 0) {
+                $('.header').addClass('fixed');
+            }else{
+                $('.header').removeClass('fixed');
+            }
+    
+            if(scrollTop > section5 - 50) {
+                $('.nav_a').addClass('white');
+            }else{
+                $('.nav_a').removeClass('white');
+            }
+    
+            if(scrollTop > section5 - 60){
+                $('.nv4').addClass('on');
+    
+            }else if(scrollTop > $('.sc4').offset().top - 50){
+                $('.nv3').addClass('on');
+    
+            }else if(scrollTop > section2 - 60){
+                $('.nv2').addClass('on');
+    
+            }else if(scrollTop > section1){
+                $('.nv1').addClass('on');
+    
+            }else if(scrollTop == '0'){
+                $('.nav_a').removeClass('on');
+            }
+    
+        });
+    
+        $('.header .logo_a').click(function(){
+            $('body, html').stop().animate({scrollTop: '0'}, 500);
+            return false;
+        });
+    
+        $('.header .nav_li').click(function(){
+            var idx = $(this).index();
+    
+    
+            if(idx == '0'){
+                $('body, html').stop().animate({scrollTop: 0}, 500);
+            }else if(idx == '1'){
+                $('body, html').stop().animate({scrollTop: section2 - 50 + 'px'}, 500);
+            }else if(idx == '2'){
+                $('body, html').stop().animate({scrollTop: section4 - 120 + 'px'}, 500);
+            }else if(idx == '3'){
+                $('body, html').stop().animate({scrollTop: section5 - 50 + 'px'}, 500);
+            }
+    
+            return false;
+        });
+    
+        $('.sc1 .view_btn').click(function(){
+            $('body, html').stop().animate({scrollTop: section4 - 120 + 'px'}, 1000);
+        });
+    
+    
+        $('.sc4 .port_box .btn').mouseenter(function(){
+            $(this).find('.bar').stop().animate({'width': '55%'}, 300);
+        });
+        $('.sc4 .port_box .btn').mouseleave(function(){
+            $(this).find('.bar').stop().animate({'width': '0%'}, 300);
+        });
+    
+    
+        $('.port_box .img_area, .port_box .text_area').click(function(){
+            if($(this).parent().hasClass('exit')){
+                location.href='exit.html';
+            }
+            else if($(this).parent().hasClass('nest')){
+                location.href='nest.html';
+            }
+            else if($(this).parent().hasClass('medifine')){
+                location.href='medifine.html';
+            }
+            else if($(this).parent().hasClass('lawpaper')){
+                location.href='lawpaper.html';
+            }
+            else if($(this).parent().hasClass('smmakers')){
+                location.href='smmakers.html';
+            }
+            else if($(this).parent().hasClass('moja')){
+                location.href='moja.html';
+            }
+            else if($(this).parent().hasClass('incom')){
+                location.href='incom.html';
+            }
+            else if($(this).parent().hasClass('gain')){
+                location.href='gain.html';
+            }
+            else if($(this).parent().hasClass('trip')){
+                location.href='trip.html';
+            }
+            else if($(this).parent().hasClass('detail')){
+                location.href='detail.html';
+            }
+            else if($(this).parent().hasClass('design')){
+                location.href='design.html';
+            }
+            else if($(this).parent().hasClass('hook')){
+                location.href='hook.html';
+            }
+    
+            return false;
+        });
+    
+        $('.sc4 .category .cate_a').click(function(){
+            
+            $('.cate_a').removeClass('on');
+            $(this).addClass('on');
+            var idx = $(this).parent().index();
+    
+            $('.sc4 .port_box').removeClass('on');
+    
+            if(idx == '0'){
+                $('.sc4 .port_wrap .cate1').addClass('on');
+                $('.sc4 .port_wrap .cate2').addClass('on');
+            }
+            else if(idx == '1'){
+                $('.sc4 .port_wrap .cate1').addClass('on');
+            }
+            else if(idx == '2'){
+                $('.sc4 .port_wrap .cate2').addClass('on');
+            }
+    
+            return false
+        });
         
-        $('.cate_a').removeClass('on');
-        $(this).addClass('on');
-        var idx = $(this).parent().index();
-
-        $('.sc4 .port_box').removeClass('on');
-
-        if(idx == '0'){
-            $('.sc4 .port_wrap .cate1').addClass('on');
-            $('.sc4 .port_wrap .cate2').addClass('on');
-        }
-        else if(idx == '1'){
-            $('.sc4 .port_wrap .cate1').addClass('on');
-        }
-        else if(idx == '2'){
-            $('.sc4 .port_wrap .cate2').addClass('on');
-        }
-
-        return false
-    });
     
-
-    var winW = $(window).innerWidth();
-    var footer = parseInt($('.footer').innerHeight());
-    
-    detectBottom();
-
-    $(document).scroll(function(){
+        var winW = $(window).innerWidth();
+        var footer = parseInt($('.footer').innerHeight());
+        
         detectBottom();
-    });
-
-    $(window).resize(function(){
-        detectBottom();
-    });
-
-    $('.gotop').click(function(){
-        $('body, html').stop().animate({scrollTop: '0'}, 500);
-    });
-
-
-    gsap.set(".sc2 .inner", {opacity: 0});
-
-    gsap.to(".sc2 .inner", {
-        scrollTrigger: {
-          trigger: ".sc2 .inner",
-          start: "-60% top",
-          onEnter:() => {
-            gsap.to(".sc2 .inner", {opacity:"1", duration:0.5, y: -20})
-          },
-          onLeaveBack:() => {
-                gsap.to(".sc2 .inner", {opacity:"0", duration:0.5, y: 0})
-          }
-        },
-      });
-
-      gsap.set(".sc3 .h2", {opacity: 0});
-      gsap.set(".sc3 .career_bg", {opacity: 0});
-
-      gsap.to(".sc3 .h2", {
-        scrollTrigger: {
-          trigger: ".sc3 .h2",
-        //   start: "-70% top",
-          onEnter:() => {
-            gsap.to(".sc3 .h2", {opacity:"1", duration:0.5, y: -20})
-          },
-          onLeaveBack:() => {
-                gsap.to(".sc3 .h2", {opacity:"0", duration:0.5})
-          }
-        },
-      });
-
-      gsap.to(".sc3 .career_bg", {
-        scrollTrigger: {
-          trigger: ".sc3 .career_bg",
-          start: "-100% top",
-          onEnter:() => {
-            gsap.to(".sc3 .career_bg", {opacity:"1", duration:0.5, delay: 0.5, y: -20})
-          },
-          onLeaveBack:() => {
-                gsap.to(".sc3 .career_bg", {opacity:"0", duration:0.5})
-          }
-        },
-      });
-
-
-      gsap.set(".sc4", {scale: 0.6});
-      gsap.to(".sc4", {
-        scrollTrigger: {
-          trigger: ".sc4",
-          start: "-80% top",
-          end: "top top",
-          scrub: 0.5,
-        },
-          scale: 1,
-          ease: "none", 
-      });
-
-
     
-
-});
+        $(document).scroll(function(){
+            detectBottom();
+        });
+    
+        $(window).resize(function(){
+            detectBottom();
+        });
+    
+        $('.gotop').click(function(){
+            $('body, html').stop().animate({scrollTop: '0'}, 500);
+        });
+    
+    
+        gsap.set(".sc2 .inner", {opacity: 0});
+    
+        gsap.to(".sc2 .inner", {
+            scrollTrigger: {
+              trigger: ".sc2 .inner",
+              start: "-60% top",
+              onEnter:() => {
+                gsap.to(".sc2 .inner", {opacity:"1", duration:0.5, y: -20})
+              },
+              onLeaveBack:() => {
+                    gsap.to(".sc2 .inner", {opacity:"0", duration:0.5, y: 0})
+              }
+            },
+          });
+    
+          gsap.set(".sc3 .h2", {opacity: 0});
+          gsap.set(".sc3 .career_bg", {opacity: 0});
+    
+          gsap.to(".sc3 .h2", {
+            scrollTrigger: {
+              trigger: ".sc3 .h2",
+            //   start: "-70% top",
+              onEnter:() => {
+                gsap.to(".sc3 .h2", {opacity:"1", duration:0.5, y: -20})
+              },
+              onLeaveBack:() => {
+                    gsap.to(".sc3 .h2", {opacity:"0", duration:0.5})
+              }
+            },
+          });
+    
+          gsap.to(".sc3 .career_bg", {
+            scrollTrigger: {
+              trigger: ".sc3 .career_bg",
+              start: "-100% top",
+              onEnter:() => {
+                gsap.to(".sc3 .career_bg", {opacity:"1", duration:0.5, delay: 0.5, y: -20})
+              },
+              onLeaveBack:() => {
+                    gsap.to(".sc3 .career_bg", {opacity:"0", duration:0.5})
+              }
+            },
+          });
+    
+    
+          gsap.set(".sc4", {scale: 0.6});
+          gsap.to(".sc4", {
+            scrollTrigger: {
+              trigger: ".sc4",
+              start: "-80% top",
+              end: "top top",
+              scrub: 0.5,
+            },
+              scale: 1,
+              ease: "none", 
+          });
+    });
 
 
 function detectBottom() {
